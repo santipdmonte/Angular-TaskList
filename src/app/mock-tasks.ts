79ece6342d6181fa44a69ea702e2ef76329282be
santipdmonte <santiagopedemonte02@gmail.com>
2022-05-05 19:21:56 -0300

    mock y task

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task} from './Task';
+
+export const TASKS:Task[] = [
+    {
+        id: 1,
+        text: "terminar primer modulo de Angular",
+        day: "5 de la tarde a las 12pm",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Pasear al perro",
+        day: "2 de la tarde a las 12pm",
+        reminder: true
+    },
+    {
+        id: 4,
+        text: "Ver netflix",
+        day: "8pm en adelante",
+        reminder: false
+    }
+]
