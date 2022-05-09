423bd5675e09fa953be3ad20590dfe4275d70ff0
santipdmonte <santiagopedemonte02@gmail.com>
2022-05-09 11:35:59 -0300

    Task reminder + actualizacion en base de datos

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHandler} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable, of} from 'rxjs';
 import { Task } from '../Task';
 import { TASKS } from '../mock-tasks'
 
+
+const httpOptions = {
+  headers: new HttpHeaders ({
+    'Content-Type':'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +28,11 @@ export class TaskService {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.delete<Task>(url)
   }
+
+  updateTaskreminder(task: Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions)
+  }
+  
 
 }
